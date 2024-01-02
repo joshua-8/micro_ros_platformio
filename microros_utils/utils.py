e1ab0b5a770a289af2bb58734a853dc929d593d7
joshua-8 <joshua_phelps@brown.edu>
2024-01-02 03:17:53 -0800

    add prints

--- microros_utils/utils.py
+++ microros_utils/utils.py
@@ -1,6 +1,9 @@
 import subprocess
 
 def run_cmd(command, env=None):
+    print("version")
+    print(sys.version)
+    print("---")
     return subprocess.run(command,
         capture_output = True,
         shell = True,
