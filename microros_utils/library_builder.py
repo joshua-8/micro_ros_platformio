f1ddd75df12aad2fa0e668774e96cedc3cf38244
joshua-8 <joshua_phelps@brown.edu>
2024-01-02 04:12:34 -0800

    remove .

--- microros_utils/library_builder.py
+++ microros_utils/library_builder.py
@@ -115,7 +115,7 @@ class Build:
         print("----------------------------------------")
         print(self.env)
         print("----------------------------------------")
-        command = "cd {} && . {} && colcon build --cmake-args -DBUILD_TESTING=OFF -DPython3_EXECUTABLE=`which python`".format(self.dev_folder, self.python_env)
+        command = "cd {} && {} && colcon build --cmake-args -DBUILD_TESTING=OFF -DPython3_EXECUTABLE=`which python`".format(self.dev_folder, self.python_env)
         print(command)
         print("----------------------------------------")
         result = run_cmd(command, env=self.env)
@@ -192,7 +192,7 @@ class Build:
 
         common_meta_path = self.library_folder + '/metas/common.meta'
         colcon_command = '. {} && colcon build --merge-install --packages-ignore-regex=.*_cpp --metas {} {} {} --cmake-args -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=OFF  -DTHIRDPARTY=ON  -DBUILD_SHARED_LIBS=OFF  -DBUILD_TESTING=OFF  -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE={} -DPython3_EXECUTABLE=`which python`'.format(self.python_env, common_meta_path, meta_file, user_meta, toolchain_file)
-        command = "cd {} && . {}/install/setup.sh && {}".format(self.mcu_folder, self.dev_folder, colcon_command)
+        command = "cd {} && {}/install/setup.sh && {}".format(self.mcu_folder, self.dev_folder, colcon_command)
         result = run_cmd(command, env=self.env)
 
         if 0 != result.returncode:
